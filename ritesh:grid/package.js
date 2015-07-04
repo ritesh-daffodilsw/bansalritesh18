6156181a1c84d31c01598913b694dd093068ac3e
ritesh-daffodilsw <ritesh.bansal@daffodilsw.com>
2015-07-04 11:35:33 +0530

    updates1

--- ritesh:grid/package.js
+++ ritesh:grid/package.js
@@ -2,7 +2,7 @@ Package.describe({
 
   summary: "What this does",
 
-  version: "1.0.2",
+  version: "1.0.3",
 
   name: "ritesh:grid",
   git : "https://github.com/ritesh-daffodilsw/bansalritesh18/ritesh:riteshgrid"  
